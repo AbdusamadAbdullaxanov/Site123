4393a55a4ba60c24d3bbeffd242ada3d83b019a0
Abdusamad <100060748+AbdusamadAbdullaxanov@users.noreply.github.com>
2023-04-23 18:23:30 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qz7^%8#bomn4dn)w*27tq^qwh$+a9v6yw)q%@)g3v(6pqz(u+5'
 
-CSRF_TRUSTED_ORIGINS = ["*"],
+CSRF_TRUSTED_ORIGINS = "web-production-72a1.up.railway.app",
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
